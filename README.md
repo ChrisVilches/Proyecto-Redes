25914c60fc1eca26c79fc10ce040ee51341b0c86
FeloVilches <felovilches@gmail.com>
2015-12-23 20:36:28 -0300

    arregle varias cosas

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Proyecto-Redes
+# Proyecto Redes
+
+## Archivos
+
+1. config.php: configuracion de servidores (nombre servidor, login, puerto, etc). Puede cambiarse la configuracion a cualquier servidor.
+2. ftp_ajax.php: archivo que recibe una peticion AJAX, con el archivo, para subirlo al servidor FTP.
+3. index.php: contiene la interfaz de usuario, y formulario.
+
+## Caracteristicas
+
+1. Logra implementar el protocolo FTP, subiendo archivos al servidor.
+2. Puede configurarse con cualquier servidor. La aplicacion no depende de un servidor en especifico, puede usar servidores externos, etc (no solo localhost).
+3. Usa AJAX
--- ftp_ajax.php
+++ ftp_ajax.php
@@ -2,34 +2,25 @@
 
 	require_once("config.php");
 
+	// Si no se pasaron archivos, dejar de ejecutar
 	if(isset($_FILES) && sizeof($_FILES) == 0){		
-		die();
+		exit;
 	}
 
+	// Conectarse al servidor
 	$ftp_connection = ftp_connect(ftp_config::$server, ftp_config::$port);
 	$ftp_login = ftp_login($ftp_connection, ftp_config::$username, ftp_config::$password);
 
-
+	// Validar conexion o login
 	if (!$ftp_connection || !$ftp_login) { 
-	    echo "FTP connection has failed!";
-	    echo "Attempted to connect"; 
 	    exit; 
 	}
 
-	var_dump($_FILES);
-
-
+	// Realizar la subida de archivo
 	$source_file = $_FILES["filename"]["tmp_name"];
 	$filename = $_FILES["filename"]["name"]; 
 	$upload = ftp_put($ftp_connection, $filename, $source_file, FTP_BINARY);
 
 	ftp_close($ftp_connection); 
-
-$myfile = fopen("newfile.txt", "w") or die("Unable to open file!");
-	$txt = "John Doea kakakakaka\n";
-	fwrite($myfile, $txt);
-	$txt = "Jane Doe\n";
-	fwrite($myfile, $txt);
-	fclose($myfile);
 
 ?>
