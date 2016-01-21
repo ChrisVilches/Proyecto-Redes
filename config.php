c0dee0d2d48a862655983b374b10705c119d9e14
felo <felovilches@gmail.com>
2016-01-20 23:55:15 -0300

    listo

--- config.php
+++ config.php
@@ -2,16 +2,16 @@
 
 	class ftp_config{
 
-		public static $server = "127.0.0.1";
+		public static $server = "181.74.141.110";//"ftp.redes.comp";
 		public static $port = 21;
-		public static $username = "daemon";
-		public static $password = "xampp";
+		public static $username = "juanito";//"daemon";
+		public static $password = "redes";//"xampp";
 
 	}
 
 	class ntp_config{
 
-		public static $server = "1.south-america.pool.ntp.org";
+		public static $server = "179.4.144.85";//"pancho.redes.comp";//"1.south-america.pool.ntp.org";
 		public static $port = 123;
 		public static $timezone = "Chile/Continental";	// Esto es valido en PHP
 
